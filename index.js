f41f5c46983ab017a167045b5780022b0dba8548
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-03 19:42:11 +0600

    add html and javascript

---
+++ index.js
@@ -0,0 +1,33 @@
+var fristName ="ahsaul";
+var lastName =" huq <br/>";
+var num ="0175313";
+var num1 ="8080 <br/>";
+var address ="brahmanbari<br/><br/>";
+
+document.write(fristName + lastName + num +num1 + address);
+
+
+var fullName ="ahsanul";
+var lastName ="huq<br/>"
+
+document.write("This is " + fullName + lastName);
+
+var nam=" shibu";
+var kam="ahsanul"
+var sam=" huq <br/>";
+
+document.write(kam + sam + nam);
+
+
+var shibu="ashanu";
+var ahsa=" huq";
+var huq=" shibu "
+
+document.write(shibu + ahsa + huq +" is my name<br/><br/>");
+
+var num2="20<br/>";
+var num3="50<br/>";
+var num4="50<br/>";
+
+
+document.write("number="+num2+" number="+num3+" number="+num4+"  equal=140");
